8b945ed77d3cefa6cdb210cbc72eace840968b5a
CamilaGon <cg561351@gmail.com>
2022-11-16 03:14:52 -0300

    Corrige lint

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
-export class Search extends Component {
+class Search extends Component {
   state = {
     artistInput: '',
     loading: false,
