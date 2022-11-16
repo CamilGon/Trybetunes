1d7af3964a6e0ba7e9abebf1fbe79164791d1dc8
CamilaGon <cg561351@gmail.com>
2022-11-16 00:04:11 -0300

    Requisito 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  state = {
+    userName: '',
+    loading: true,
+  };
+
+  async componentDidMount() {
+    const { name } = await getUser();
+    this.setState({
+      userName: name,
+      loading: false,
+    });
+  }
+
   render() {
+    const { userName, loading } = this.state;
     return (
-      <div>Header</div>
+      <header data-testid="header-component">
+        { loading === true ? (<div>Carregando...</div>)
+          : (<div data-testid="header-user-name">{userName}</div>) }
+      </header>
     );
   }
 }
