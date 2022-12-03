a2f50c29b5545ebf9170a685131c9db4a10bb71b
CamilaGon <cg561351@gmail.com>
2022-12-03 11:52:53 -0300

    Requisito 7

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Loading from './Loading';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+
+class MusicCard extends React.Component {
+  state = {
+    load: false,
+    getFave: [],
+  };
+
+  async componentDidMount() {
+    this.handleFavorites();
+  }
+
+  handleFavorites = async () => {
+    this.setState({
+      load: true,
+    });
+    const response = await getFavoriteSongs();
+    this.setState({
+      load: false,
+      getFave: response,
+    });
+  };
+
+  handleChange = async ({ target }) => {
+    const { music } = this.props;
+    const { checked } = target;
+    if (checked) {
+      this.setState({
+        load: true,
+      });
+      await addSong(music);
+    } else {
+      this.setState({
+        load: true,
+      });
+      await removeSong(music);
+    }
+    const result = await getFavoriteSongs();
+    this.setState({
+      load: false,
+      getFave: result,
+    });
+  };
+
+  render() {
+    const {
+      trackName,
+      previewUrl,
+      trackId,
+    } = this.props;
+    const {
+      load,
+      getFave,
+    } = this.state;
+    return (
+      <div>
+        {load ? <Loading /> : (
+          <div>
+            <h1>{trackName}</h1>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              <code>audio</code>
+
+            </audio>
+            <label htmlFor={ trackId }>
+              Favoritar
+              <input
+                data-testid={ `checkbox-music-${trackId}` }
+                type="checkbox"
+                name={ trackId }
+                id={ trackId }
+                checked={ getFave
+                  .some((music) => music.trackId === trackId) }
+                onChange={ this.handleChange }
+              />
+            </label>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+}.isRequired;
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,81 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
-export default class Album extends Component {
+class Album extends Component {
+  state = {
+    song: {},
+    album: [],
+    favoriteSong: [],
+  };
+
+  componentDidMount() {
+    this.requestMusic();
+  }
+
+  requestMusic = async () => {
+    const { match: { params: { id } } } = this.props;
+    const response = await getMusics(id);
+    const allMusic = [...response];
+    const music = allMusic.shift();
+    const favoriteMusic = await getFavoriteSongs();
+    this.setState({
+      song: music,
+      album: allMusic,
+      favoriteSong: favoriteMusic,
+    });
+  };
+
   render() {
+    const { song, album, favoriteSong } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <div>
+          <div className="container-album">
+            <span
+              className="album-name"
+              data-testid="album-name"
+            >
+              { song.collectionName }
+            </span>
+            <span
+              className="artist-name"
+              data-testid="artist-name"
+            >
+              { song.artistName }
+            </span>
+          </div>
+
+          <div className="music-list">
+            {
+              album.map((music) => (
+                <MusicCard
+                  key={ music.trackId }
+                  single={ music }
+                  trackName={ music.trackName }
+                  previewUrl={ music.previewUrl }
+                  trackId={ music.trackId }
+                  musicSave={ favoriteSong }
+                />
+              ))
+            }
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default Album;
