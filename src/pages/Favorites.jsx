628734a0d38ba1b5f215d85ba12eb4bd87054aea
CamilaGon <cg561351@gmail.com>
2022-12-06 00:31:58 -0300

    Requisito 13 e mmetade do 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
+import PropTypes from 'prop-types';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
 
-export default class Favorites extends Component {
+export class Favorites extends Component {
+  state = {
+    load: false,
+    storedFavs: [],
+  };
+
+  componentDidMount() {
+    this.handleListFave();
+  }
+
+  handleListFave = async () => {
+    this.setState({
+      load: true,
+    });
+    const response = await getFavoriteSongs();
+    this.setState({
+      load: false,
+      storedFavs: response,
+    });
+  };
+
   render() {
+    const {
+      storedFavs,
+      load,
+    } = this.state;
     return (
       <div data-testid="page-favorites">
-        <Header />
-        Favorites
+        { load ? <Loading /> : (
+          storedFavs.map(({
+            trackName,
+            trackNumber,
+            trackId,
+            previewUrl,
+          }) => (
+            <div key={ trackName }>
+              <MusicCard
+                trackName={ trackName }
+                trackNumber={ trackNumber }
+                trackId={ Number(trackId) }
+                previewUrl={ previewUrl }
+                musicIndex={ storedFavs }
+              />
+            </div>
+          )))}
       </div>
     );
   }
 }
+Favorites.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+}.isRequired;
+export default Favorites;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,65 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 export default class Profile extends Component {
+  state = {
+    load: false,
+    users: [],
+  };
+
+  componentDidMount() {
+    this.saveUsersLog();
+  }
+
+  saveUsersLog = async () => {
+    this.setState({
+      load: true,
+    });
+    const loggedPerson = await getUser();
+    this.setState({
+      load: false,
+      users: loggedPerson,
+    });
+  };
+
   render() {
+    const {
+      load,
+      users,
+    } = this.state;
+    const {
+      image,
+      description,
+      email,
+      name,
+    } = users;
     return (
       <div data-testid="page-profile">
         <Header />
         Profile
+        { load ? <Loading /> : (
+          <div>
+            <img
+              data-testid="profile-image"
+              src={ image }
+              alt={ name }
+            />
+            <Link
+              to="/profile/edit"
+            >
+              Editar perfil
+            </Link>
+            <span>Nome</span>
+            <h1>{name}</h1>
+            <span>E-mail</span>
+            <h2>{email}</h2>
+            <span>Descrição</span>
+            <h3>{description}</h3>
+          </div>
+        )}
       </div>
     );
   }
